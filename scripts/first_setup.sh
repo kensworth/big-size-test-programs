7bf5581796e09cb5761342a20847a9d897afc953
Curtis Li <curtis.li@nyu.edu>
2016-12-11 09:28:09 +0000

    fixed proto scripts

--- scripts/first_setup.sh
+++ scripts/first_setup.sh
@@ -2,5 +2,5 @@ virtualenv env
 source env/bin/activate
 pip install -U pip
 pip install -Ur misc/requirements.txt
-python -m grpc.tools.protoc -I. --python_out=proto/ --grpc_python_out=proto/ proto/*.proto
+./scripts/make_proto.sh
 deactivate
