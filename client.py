67eb0e999715e00e4bbd4797d8f36fffed03c7ba
Curtis Li <curtis.li@nyu.edu>
2016-12-11 04:10:30 +0000

    renamed files

---
+++ client.py
@@ -0,0 +1,38 @@
+import code_eval_pb2
+import grpc
+import sys
+
+address = "localhost"
+port = 8000
+
+
+def main():
+  if len(sys.argv) < 1:
+    print("Not enough arguments.")
+    printUsage()
+    sys.exit(1)
+
+  f = open(sys.argv[1], 'r')
+  code = f.read()
+
+  channel = grpc.insecure_channel('%s:%d' % (address, port))
+  stub = code_eval_pb2.CodeEvaluatorStub(channel)
+
+  request = code_eval_pb2.EvalRequest()
+  request.code = code
+
+  # Query the server
+  reply = stub.Eval(request)
+
+  # Print the response
+  print(reply.response)
+
+
+# Print how to start program via command-line
+def printUsage():
+  print("Usage: python %s \"def fib(n): ...\"" % sys.argv[0])
+
+
+# Run program if run as main
+if __name__ == "__main__":
+  main()
---
+++ example_fib_code.py
@@ -0,0 +1,9 @@
+def fib(n):
+  if n < 1:
+    return None
+  fib_matrix = [None] * (n + 1)
+  fib_matrix[0] = 1
+  fib_matrix[1] = 1
+  for i in range(2, n):
+    fib_matrix[i] = fib_matrix[i - 1] + fib_matrix[i - 2]
+  return fib_matrix[n - 1]
