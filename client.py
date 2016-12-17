15fc731c63a11f8a2474244e5ab17b4e9376f643
Curtis Li <curtis.li@nyu.edu>
2016-12-17 21:15:56 +0000

    update client

--- client.py
+++ client.py
@@ -3,26 +3,53 @@ import subprocess
 import grpc
 import sys
 import time
+import boto3
 
 address = "localhost"
 port = 8000
 
+sqs_url = "https://sqs.us-east-1.amazonaws.com/542342679377/SubmissionQueue"
+sqs = boto3.resource('sqs')
+queue = sqs.get_queue_by_name(QueueName='SubmissionQueue')
 
-def main():
-    if len(sys.argv) < 1:
-        print("Not enough arguments.")
-        printUsage()
-        sys.exit(1)
+def serve():
+    start_docker()
 
-    f = open(sys.argv[1], 'r')
-    code = f.read()
+    while True:
+        for message in queue.receive_messages(
+            MessageAttributeNames=['Submission'],
+            WaitTimeSeconds=20
+        ):
+            code = ''
+            if message.message_attributes is not None:
+                code = message.message_attributes.get('Submission').get('StringValue')
 
+            print('Got code: {0}'.format(code))
+
+            test(code)
+
+            # Let the queue know that the message is processed
+            message.delete()
+
+            start_docker()
+
+
+def start_docker():
+    # Run ./scripts/run_docker.sh
     p = subprocess.Popen(["sh", "scripts/run_docker.sh"]).wait()
     time.sleep(1)
 
+def stop_docker():
+    # Run ./scripts/kill_docker.sh
+    p = subprocess.Popen(["sh", "scripts/kill_docker.sh"]).wait()
+    time.sleep(1)
+
+def test(code):
+    # Connect to server
     channel = grpc.insecure_channel('%s:%d' % (address, port))
     stub = code_eval.CodeEvaluatorStub(channel)
 
+    # Form request
     request = code_eval.EvalRequest()
     request.code = code
 
@@ -40,4 +67,4 @@ def printUsage():
 
 # Run program if run as main
 if __name__ == "__main__":
-    main()
+    serve()
